9b1703ee08c57ac50014f1fd5d2e15a5021797e0
Akifumi Kamata <akifumi.kamata@a4works.co.jp>
2014-08-29 16:39:02 +0900

    初版

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ github2chatwork
 ===============
 
 chatworkにPullRequestを通知します。
+
+簡易版なので、認証やコメント等には対応していません。
+
+#使い方
+
+##サーバー側の設定
+外部からリクエスト可能な場所にgithub2cw.phpを設置します。  
+CHATWORK_APIKEY にchatworkのAPIキーを設定します。  
+
+##GitHubの設定
+GitHubの「Webhooks & Services」から、Payload URLを設定します。
+http://example.com/github2cw.php?rid={RoomID}
+
+Which events would you like to trigger this webhook?の項目で
+Let me select individual events.を選択し、Pull Request にチェックをいれます。
+
+WebhooksのRedeliver機能など適宜利用していただき、動作を確認してください。
+
+###chatwork API
+APIの利用には申請が必要となります。  
+下記URLからAPIの利用申請を行ってください。  
+http://developer.chatwork.com/ja/
+
---
+++ github2cw.php
@@ -0,0 +1,112 @@
+<?php
+/**
+* GithubからのPull Requestをchatworkに通知します。
+* 簡易版なので、コメント通知などは対応していません。
+*/
+
+
+// APIキーを設定してください
+define("CHATWORK_APIKEY",'YOUR_APIKEY');
+
+
+//------------------------------------------------------------------------------
+
+define('CW_ENDPOINT_URL',"https://api.chatwork.com/v1/rooms/%%ROOM_ID%%/messages");
+
+
+$cwRoomId = $_REQUEST['rid'];
+
+// Payloadを取得
+$payload = file_get_contents('php://input');
+
+$ary = json_decode($payload,true);
+
+if(isset($ary["pull_request"])){
+
+	$action    = $ary["action"];
+	$url       = $ary["pull_request"]["url"];
+	$title     = $ary["pull_request"]["title"];
+	$user      = $ary["pull_request"]["user"]["login"];
+	$merged_at = $ary["pull_request"]["merged_at"];
+
+	/*
+	error_log("action:".$action);
+	error_log("url:".   $url   );
+	error_log("title:". $title );
+	error_log("user:".  $user  );
+	error_log("merged_at:".  $merged_at  );
+	*/
+
+	if($action == 'opened'){
+		$type = "GitHub - Created Pull Request.";
+	} elseif($action == "closed"){
+		if($merged_at){
+			$type = "GitHub - Merged Pull Request.";
+		} else{
+			$type = "GitHub - Closed Pull Request.";
+		}
+	} elseif($action == "created"){
+		print "Not message send. コメント通知は非対応です。";
+		exit;
+
+	} else{
+		print "Not message send.";
+		exit;
+	}
+
+	$message = "----------\n".$type."\n".$url."\n by ".$user."\n".$title;
+
+	// メッセージを送信
+	chatworkApiSendMessage($cwRoomId,$message);
+
+	print "Message send.";
+}
+
+exit;
+
+//------------------------------------------------------------------------------
+
+/**
+* chatworkAPI連携
+* メッセージの送信
+*/
+function chatworkApiSendMessage($cwRoomId,$message){
+
+	$message = mb_strimwidth($message,0,300,"...",'UTF-8');
+
+	$endpointURL = str_replace('%%ROOM_ID%%', $cwRoomId, CW_ENDPOINT_URL);
+
+	$aryData = array('body'=>$message);
+
+	$data = http_build_query($aryData);
+
+	$cwApikey = CHATWORK_APIKEY;
+
+	$opts = array(
+		'http'=>array(
+			'method'=>"POST",
+			'header'=>"X-ChatWorkToken: {$cwApikey}\r\n",
+			"content" => $data
+	)
+	);
+
+	$context = stream_context_create($opts);
+
+	// chatworkにメッセージ送信
+	$resutl = file_get_contents($endpointURL, false, $context);
+
+	print "REQUESTED.";
+
+	if($resutl){
+	    print "OK:".$resutl;
+	} else{
+	    print "NG:".$http_response_header[0];
+	}
+
+	print "<br />";
+	print "send message:{$message}<br />";
+
+	return true;
+}
+
+
